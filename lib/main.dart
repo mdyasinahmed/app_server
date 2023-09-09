963e1131a462b1836a79f4b6f1db421b4a79e8ca
mdyasinahmed <yasinahmed7875@gmail.com>
2023-09-09 21:15:51 +0600

    Add My App and Material App class

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,34 @@
 import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
 
+// main function
 void main() {
-  runApp();
+  runApp(const MyApp());
 }
+
+
+// My App and Material App
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(home: HomeActivity());
+  }
+}
+
+class HomeActivity extends StatelessWidget {
+  const HomeActivity({super.key});
+
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+
+    );
+  }
+
+
+}
+
+
 
